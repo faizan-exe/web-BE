4d5e97806d4979665df0c6a2e51f0f323aa4f5e8
RyanSikandar <ryansikandar@gmail.com>
2024-12-28 21:13:17 +0500

    changes

--- models/Ads.js
+++ models/Ads.js
@@ -7,7 +7,7 @@ const AdsSchema = new mongoose.Schema({
   description: { type: String, required: true },
   contactEmail: { type: String, required: true, match: /.+\@.+\..+/ }, // Valid email format
   contactPhone: { type: String, required: true },
-  image: { type: String, required: true }, // URL of the mentor's image
+  image: { type: String, required: false }, // URL of the mentor's image
   experience: { type: Number, required: true, min: 0 }, // Experience in years
   location: { type: String, required: true },
   availableSlots: { type: String, required: true },
