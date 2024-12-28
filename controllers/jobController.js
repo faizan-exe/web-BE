56a6cb4c02cc087bbdbe2268fb966178132b71cc
RyanSikandar <ryansikandar@gmail.com>
2024-12-28 22:31:45 +0500

    chore: added validation for jobs

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -59,6 +59,9 @@ exports.createJob = async (req, res) => {
 exports.getJobs = async (req, res) => {
     try {
         const jobs = await Job.find();
+        if (!jobs) {
+            return res.status(404).json({ message: 'No jobs found' });
+        }
         res.status(200).json(jobs);
     } catch (error) {
         res.status(500).json({ message: 'Server error', error });
