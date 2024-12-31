0489c72ed2e02eab85cda111b309b4da8b013d8e
waqas2714 <waqasali00123@gmail.com>
2024-12-31 11:22:42 +0500

    specific user added

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,6 +50,22 @@ exports.getUser = async (req, res) => {
     }
 }
 
+exports.getSpecificUser = async (req, res) => {
+  try {
+    const {id} = req.params;
+
+      const user = await User.findById(id).select('-password');
+
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      
+      res.status(200).json({ user });
+  }
+  catch (error) {
+      res.status(500).json({ message: 'Server error' });
+  }
+}
+
+
 exports.updateUserProfile = async (req, res) => {
     try {
       const { name, email, role, image } = req.body;  // Get user data from request body
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getUser, updateUserProfile } = require('../controllers/authController');
+const { register, login, getUser, updateUserProfile, getSpecificUser } = require('../controllers/authController');
 const { authenticate } = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/user', authenticate, getUser);
+router.get('/user/:id', authenticate, getSpecificUser);
 router.put('/user', authenticate, updateUserProfile);
 
 module.exports = router;
